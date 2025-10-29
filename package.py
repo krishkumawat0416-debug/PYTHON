c3e02c3577b9ea3873187fb7153ba481f2df85c6
krishkumawat0416-debug <krishkumawat0416@gmail.com>
2025-10-29 20:50:35 +0530

    Add files via upload

---
+++ package.py
@@ -0,0 +1,14 @@
+# modules : modules are the python files in which all the function and other codes written.
+# ex. hello.py (module)
+
+# package : packages are the collection of related modules which contains __init__.py files.
+# packages
+# calculator (package)
+# __init__.py
+# math_ops.py
+# --> add, sub, multi, div
+
+# calculator.py
+# from calculator import add, sub, multi, div
+# add(4,5)
+
